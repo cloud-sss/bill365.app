12ea8fa9553333dbf3b30f20e2f0d01c57c4be0e
somnathsynergic <somnath@synergic.in>
2025-11-04 10:27:02 +0530

    commit with prints

--- api/user.py
+++ api/user.py
@@ -268,6 +268,7 @@ async def login(data_login:UserLogin):
         cursor.execute(query)
         records = cursor.fetchone()
         result1 = createResponse(records, cursor.column_names, 0)
+        print(query,'requested query')
         print(result1,'result1')
         conn.close()
         cursor.close()
