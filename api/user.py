7c3a51ba01c85ba5b17fee3f0428474a33469ef4
somnathsynergic <somnath@synergic.in>
2025-11-19 17:18:12 +0530

    commit with onboarding

--- api/user.py
+++ api/user.py
@@ -343,6 +343,7 @@ async def logout(flag:LoginFlag):
 @userRouter.get('/send_otp/{phone_no}') 
 async def OTP(phone_no:int):
     otp = random.randint(1000, 9999)
+    print(otp)
     default_text = f"https://bulksms.sssplsales.in/api/api_http.php?username=SYNERGIC&password=SYN@526RGC&senderid=SYNGIC&to={phone_no}&text=OTP for mobile verification is {otp}. This code is valid for 5 minutes. Please do not share this OTP with anyone.-SYNGIC&route=Informative&type=text"
     try:
         response = requests.get(default_text)
